11cbcafecdb34c3f1c94bf04d96f438078d897a3
PROB8 <blackb8r@gmail.com>
2018-03-03 11:52:43 -0500

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 ## About 
 This is an app concept for an app that would give info on historic locations throughout the park
+
+## To run
+1. `$ npm install` at root
+2. `$ cd server`
+3. `$ npm install`
+4. `$ npm start` in /server
+5. in browser `$ localhost:3001`
---
+++ server/index.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+ 
+
+ 
+app.use(express.static(path.resolve(__dirname, '../client/public')));
+
+app.get('/api/working', (req,res) => {
+    res.send('I work')
+})
+
+app.get(/^(?!\/api(\/|$))/, (req, res) => {
+    const index = path.resolve(__dirname, '../client/public', 'index.html');
+    res.sendFile(index);
+});
+
+let server;
+function runServer(port=3001) {
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            resolve();
+        }).on('error', reject);
+    });
+}
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        server.close(err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer();
+}
+
+module.exports = {
+    app, runServer, closeServer
+};
