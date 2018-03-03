5a2af5a92138ce0a7ea6dfc12c51a5e8e9932acc
Ovie <you@example.com>
2018-03-03 14:04:17 -0500

    adds geolocation to server index.js

--- server/index.js
+++ server/index.js
@@ -3,28 +3,62 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const mapItemSchema = new mongoose.Schema({
-   link: [String],
-   lon: {type:Number, required:true},
-   lat: {type:Number, required:true},
-   info: String,
-   name:String
-});
-
-
-
-const MapItem =  mongoose.model('MapItem', mapItemSchema, mapItems);
+    link: [String],
+    lon: {type:Number, required:true},
+    lat: {type:Number, required:true},
+    info: String,
+    name:String
+ });
+ const MapItem =  mongoose.model('MapItem', mapItemSchema, mapItems);
 
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 app.get('/api/working', (req,res) => {
     res.send('I work')
-})
+});
 
-
+app.push('/api/location', (req,res) => {
+    const success = position=>{
+        const currLat = position.coords.latitude;
+        const currLong = position.coords.longitude;
+        //use coords to query database
+        db.MapItem.find({})
+            .where({ //query using range
+                        lat: {
+                            $gte: currLat + 0.01,
+                            $lte: currLat - 0.01
+                        }
+                    } &&
+                    {
+                        long: {
+                            $gte: currLong + 0.01,
+                            $lte: currLong - 0.01
+                        }
+          })
+                .then(dbMapItem=> {
+                    console.log(dbMapItem);
+                    console.log("sending document back");
+                    res.json(dbMapItem);
+                })
+                .catch(function(err) {
+                    res.json(err);
+                });
+    };
+    
+    const error = err=>{
+        alert("Failed to get your position because "+err)
+    };
+    
+    const options = {
+        enableHighAccuracy: true, 
+        maximumAge        : 30000, 
+        timeout           : 27000
+      };
+    
+    const watchLoc = navigator.geolocation.watchPosition(success,error,options);
+});
 
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
     const index = path.resolve(__dirname, '../client/public', 'index.html');
